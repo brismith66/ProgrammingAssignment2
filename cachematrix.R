485fd43ff2d0b568ad71bbef6417f36db5e56c89
Brian Smith <brismith66@gmail.com>
2015-01-21 17:53:30 -0500

    R Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This contains 
+## makeCacheMatrix 
+## cacheSolve
 
-## Write a short comment describing this function
-
+## makeCacheMatrix
+##    accepts a parameter of type matrix
+##    returns a list with methods for get/set, and getinv/setinv
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(inv) { 
+    print("setsolve: hello: m is: ")
+    print(m)
+    print("setsolve: and inv is ")
+    print(inv)
+    print("setsolve: bye")
+    m <<- inv
+  }
+  getsolve <- function() { 
+    print("getsolve: hello")
+    
+    m
+  }
+  list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve 
+##    accepts a parameter of type makeCacheMatrix
+##    returns the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  print("cacheSolve: hello")
+  m <- x$getsolve()
+  print("cacheSolve: m is ")
+  print(m)
+  print("cacheSolve: m's class is " )
+  print(class(m))
+  print("cacheSolve: is.null(m) is ")
+  print(is.null(m))
+  if (!is.null(m)) {
+    print("getting cached solution")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat)
+  x$setsolve(m)
+  m
 }
